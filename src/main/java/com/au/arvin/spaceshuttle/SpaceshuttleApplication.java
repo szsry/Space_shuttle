00c4c145abbc60cd5fd7844b136780a36a78134f
szsry <szsuruoyun@gmail.com>
2017-11-25 19:05:50 +1100

    first commit

---
+++ src/main/java/com/au/arvin/spaceshuttle/SpaceshuttleApplication.java
@@ -0,0 +1,12 @@
+package com.au.arvin.spaceshuttle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceshuttleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceshuttleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/au/arvin/spaceshuttle/SpaceshuttleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.au.arvin.spaceshuttle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpaceshuttleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
